5e3098207e4a2f6a0624bee131199d9d8f24fdf8
Zachary Spiggle <zspiggle@outlook.com>
2023-03-01 15:47:15 -0500

    Added routing to pages
    To find the routes, view src/app/app-routing.module.ts

--- FitSpace/src/app/app-routing.module.ts
+++ FitSpace/src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './Pages/home-page/home-page.component';
+import { LoginPageComponent } from './Pages/login-page/login-page.component';
+import { CreatePostPageComponent } from './Pages/create-post-page/create-post-page.component';
+import { CommunityPageComponent } from './Pages/community-page/community-page.component';
+import { ProfilePageComponent } from './Pages/profile-page/profile-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: LoginPageComponent
+  },
+  {
+    path:'Home',
+    component: HomePageComponent
+  },
+  {
+    path:'Create',
+    component: CreatePostPageComponent
+  },
+  {
+    path:'Community',
+    component: CommunityPageComponent
+  },
+  {
+    path:'Profile',
+    component: ProfilePageComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
