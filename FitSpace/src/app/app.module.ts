8005fdd7c16ee7dd865b1fc7976bfdbd9d7ba1d9
Zachary Spiggle <zspiggle@outlook.com>
2023-03-01 15:36:22 -0500

    Added all pages from diagram
    Initialized all pages for prototype

--- FitSpace/src/app/app.module.ts
+++ FitSpace/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './Pages/login-page/login-page.component';
+import { HomePageComponent } from './Pages/home-page/home-page.component';
+import { CreatePostPageComponent } from './Pages/create-post-page/create-post-page.component';
+import { CommunityPageComponent } from './Pages/community-page/community-page.component';
+import { ProfilePageComponent } from './Pages/profile-page/profile-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginPageComponent
+    LoginPageComponent,
+    HomePageComponent,
+    CreatePostPageComponent,
+    CommunityPageComponent,
+    ProfilePageComponent
   ],
   imports: [
     BrowserModule,
