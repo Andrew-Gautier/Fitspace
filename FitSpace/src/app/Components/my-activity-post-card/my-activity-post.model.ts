15155a38bbad798eec0b859f3b3ffa93eccc2439
Olivia Ecock <ope00001@mix.wvu.edu>
2023-03-05 18:43:20 -0500

    Activity

---
+++ FitSpace/src/app/Components/my-activity-post-card/my-activity-post.model.ts
@@ -0,0 +1,13 @@
+export class MyActivityPostModel {
+  
+  title: string;
+  description: string;
+  img: string;
+
+  constructor(title: string, description: string, img: string){
+    this.title = title;
+    this.description = description;
+    this.img = img;
+  }
+
+}
