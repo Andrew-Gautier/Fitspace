4c20d226a0b9dd658cefd4af8a0d05ab7a8584c5
Jonathan Dofka <jtd00007@mix.wvu.edu>
2023-04-05 22:49:39 -0400

    NoComments
    Added if statement to display no comments just yet if a post has no comments

--- FitSpace/src/app/Components/comments/comments.component.ts
+++ FitSpace/src/app/Components/comments/comments.component.ts
@@ -11,11 +11,13 @@ export class CommentsComponent implements OnInit {
   comments: any[] = [
     { id: 1, postId: 1, text: 'This is great 1' },
     { id: 2, postId: 1, text: 'This is real good 2' },
+    { id: 5, postId: 1, text: 'This is very good 5'},
     { id: 3, postId: 2, text: 'This is very nice 3' },
     { id: 4, postId: 2, text: 'This is even better 4' },
-    { id: 5, postId: 3, text: 'This is amazing 5' },
+   
   ];
   postComments: any[];
+
   constructor(private route: ActivatedRoute) { this.postComments = []; }
 
   ngOnInit(): void {
