78f582e04556d6be3ccac15b794ad31bd292414b
Zachary Spiggle <zspiggle@outlook.com>
2023-04-08 18:45:37 -0400

    Integrated comment comment and databinding with new post component

--- FitSpace/src/app/Components/post-component/mock_list.ts
+++ FitSpace/src/app/Components/post-component/mock_list.ts
@@ -1,14 +1,14 @@
 export const mock_list_slides = [
   {
     img: "assets/example-posts/pushups.jpg",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in iaculis ex. Etiam volutpat laoreet urna.Morbi ut tortor nec nulla commodo malesuada sit amet vel lacus. Fusce eget efficitur libero. Morbi dapibus portaquam laoreet placerat."
+    text: "This post is missing slides."
   },
   {
-    img: "assets/example-posts/POKEMON.png",
-    text: "Test descriptions dsuoiyfhjdshijas dfshdjkkjsadhisadh hjskadha sdksadhjksa ahjdsa mdsa dhskajkjh hjsad sajhkdsahkj dhjksadhaj sdhjksa hkjad hjdshijas dfshdjkkjsadhisadh hjskadha sdksadhjksa ahjdsa mdsa dhskajkjh hjsad sajhkdsahkj dhjksadhaj sdhjksa hkjad"
+    img: "assets/example-posts/situp.png",
+    text: "Something has gone horribly wrong"
   },
   {
-    img: "assets/example-posts/CARDIOMEME.jpg",
-    text: "AHHHHHHHHHHHHHHHHHHHHHHhhhh CARDIOOOOOOOOOOOOOOOOOOOO"
+    img: "assets/Under-Construction.jpg",
+    text: "Please proceed to panic."
   }
 ]
--- FitSpace/src/app/Components/post-component/post-component.component.ts
+++ FitSpace/src/app/Components/post-component/post-component.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { mock_list_slides } from './mock_list';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-post-component',
   templateUrl: './post-component.component.html',
@@ -7,4 +8,21 @@ import { mock_list_slides } from './mock_list';
 })
 export class PostComponentComponent {
   slides = mock_list_slides;
+
+  @Input() PostTitle : string;
+  @Input() Slides : any[];
+  @Input() Username : string;
+  @Input() PostID : string;
+
+  constructor(private router: Router){
+    this.Slides = mock_list_slides;
+    this.PostTitle = "Missing Post";
+    this.Username = "Missing User";
+    this.PostID = "0";
+  }
+
+  //This routes to a new unique page based on id 
+  viewComments(id: string): void {
+    this.router.navigate(['comments', id]);
+  }
 }
