c6daa253d6359c4a4e17e69152a497dad15535bd
Olivia Ecock <ope00001@mix.wvu.edu>
2023-05-01 20:23:31 -0400

    Latest MyActivity

--- FitSpace/src/app/Pages/my-activity-page/my-activity-page.component.ts
+++ FitSpace/src/app/Pages/my-activity-page/my-activity-page.component.ts
@@ -1,7 +1,10 @@
-/* import { Component, OnInit } from '@angular/core'; */
-import { Component } from '@angular/core';
-//import { MyActivityModel } from '../../Components/my-activity/my-activity.model';
-//import { mock_list } from '../../Components/my-activity/mock_list';
+import { Component, OnInit } from '@angular/core';
+//import CanvasJS from 'canvasjs';
+//import * as CanvasJSAngularChart from '../../../assets/canvasjs.angular.component';
+//var CanvasJSChart = CanvasJSAngularChart.CanvasJSChart;
+//var CanvasJS = require('canvasjs');
+
+declare var myActivity: any;
 
 declare function getNextActivity(): any;
 declare function getNextDuration(): any;
@@ -9,16 +12,39 @@ declare function getNextDuration(): any;
 @Component({
   selector: 'app-my-activity-page',
   templateUrl: './my-activity-page.component.html',
-
   styleUrls: ['./my-activity-page.component.css']
 })
 
-export class MyActivityPageComponent {
+export class MyActivityPageComponent implements OnInit {
+  sun = 0;
+  mon = 0;
+  tue = 0;
+  wed = 0;
+  thu = 0;
+  fri = 0;
+  sat = 0;
+
+  max = 7;
+
+  activities = Array<any>;
+  durations = Array<any>;
+
   chart: any;
+
+  // Default data points (to present an initial "interesting" bar graph)
+  dps = [
+    { label: "Saturday", y: 73 },
+    { label: "Friday", y: 20 },
+    { label: "Thursday", y: 24 },
+    { label: "Wednesday", y: 29 },
+    { label: "Tuesday", y: 19 },
+    { label: "Monday", y: 33 },
+    { label: "Sunday", y: 13 }
+  ];
 
   chartOptions = {
     title: {
-      text: "Past 7 Workouts"
+      text: "Workout comparison by day"
     },
     animationEnabled: true,
     axisY: {
@@ -29,23 +55,100 @@ export class MyActivityPageComponent {
       type: "bar",
       indexLabel: "{y}",
       yValueFormatString: "#,###mins",
-      dataPoints: [
-        { label: getNextActivity(), y: getNextDuration() },
-        { label: getNextActivity(), y: getNextDuration() },
-        { label: getNextActivity(), y: getNextDuration() },
-        { label: getNextActivity(), y: getNextDuration() },
-        { label: getNextActivity(), y: getNextDuration() },
-        { label: getNextActivity(), y: getNextDuration() },
-        { label: getNextActivity(), y: getNextDuration() }
-      ]
+      dataPoints: this.dps,
     }]
   }
 
-  //cards: MyActivityModel[] = [];
+  getChartInstance(chart: object) {
+    this.chart = chart;
+    console.log("getChartInstance called");
+    //setTimeout(this.updateChart, 1000);
+    //this.chart.render();
+  }
 
-  constructor() {
-    // for (var item of mock_list) {
-    //   console.log(item);
-    //   this.cards.push(item);
+  //updateChart = () => {
+  //this.chart.render();
+  //  setTimeout(this.updateChart, 1000); //Chart updated every 1 second
+  //}
+
+  ngOnInit() {
+    myActivity = this;
+  }
+
+  ngOnDestroy(): any {
+    myActivity = null;
+  }
+
+  workoutData() {
+    var sunWork = (document.getElementById("sunday") as HTMLInputElement).value;
+    var monWork = (document.getElementById("monday") as HTMLInputElement).value;
+    var tueWork = (document.getElementById("tuesday") as HTMLInputElement).value;
+    var wedWork = (document.getElementById("wednesday") as HTMLInputElement).value;
+    var thuWork = (document.getElementById("thursday") as HTMLInputElement).value;
+    var friWork = (document.getElementById("friday") as HTMLInputElement).value;
+    var satWork = (document.getElementById("saturday") as HTMLInputElement).value;
+
+    console.log("Sunday: " + sunWork);
+    console.log("Monday: " + monWork);
+    console.log("Tuesday: " + tueWork);
+    console.log("Wednesday: " + wedWork);
+    console.log("Thursday: " + thuWork);
+    console.log("Friday: " + friWork);
+    console.log("Saturday: " + satWork);
+
+    if (sunWork == null || Number.isNaN(Number(sunWork))) { sunWork = "0"; }
+    if (monWork == null || Number.isNaN(Number(monWork))) { monWork = "0"; }
+    if (tueWork == null || Number.isNaN(Number(tueWork))) { tueWork = "0"; }
+    if (wedWork == null || Number.isNaN(Number(wedWork))) { wedWork = "0"; }
+    if (thuWork == null || Number.isNaN(Number(thuWork))) { thuWork = "0"; }
+    if (friWork == null || Number.isNaN(Number(friWork))) { friWork = "0"; }
+    if (satWork == null || Number.isNaN(Number(satWork))) { satWork = "0"; }
+
+    this.dps[0] = { label: "Sunday", y: Number(sunWork) };
+    this.dps[1] = { label: "Monday", y: Number(monWork) };
+    this.dps[2] = { label: "Tuesday", y: Number(tueWork) };
+    this.dps[3] = { label: "Wednesday", y: Number(wedWork) };
+    this.dps[4] = { label: "Thursday", y: Number(thuWork) };
+    this.dps[5] = { label: "Friday", y: Number(friWork) };
+    this.dps[6] = { label: "Saturday", y: Number(satWork) };
+
+    console.log(this.dps);
+
+    this.chart.render();
+
+    var total = Number(sunWork) + Number(monWork) + Number(tueWork) + Number(wedWork) + Number(thuWork) + Number(friWork) + Number(satWork);
+    console.log("Weekly total: " + total + " minutes");
+
+    const totalMessage: HTMLElement = (document.getElementById("totalMessage") as HTMLElement);
+  
+    totalMessage.innerHTML = "Total workout duration this week is " + total + " minutes.";
+    console.log("TotalMessage: " + totalMessage.innerHTML);
+  }
+
+  async refreshChart() {
+    
+    this.chart.options = {
+      title: {
+        text: "Workout comparison by day"
+      },
+      animationEnabled: true,
+      axisY: {
+        includeZero: true,
+        suffix: "mins"
+      },
+      data: [{
+        type: "bar",
+        indexLabel: "{y}",
+        yValueFormatString: "#,###mins",
+        dataPoints: this.dps,
+      }]
+
+    };
+
+    console.log("Refresh button pressed");
+
+    this.chart.render();
+  
+    console.log(this.chart);
   }
 }
--- FitSpace/src/assets/MyActivity.js
+++ FitSpace/src/assets/MyActivity.js
@@ -1,102 +1,85 @@
-var max = 7;                         // maximum number of activities
-var activities = new Array(max);     // initialize the arrays
-var durations = new Array(max);
-var index = 0;                       // current index
-var next = 0;                        // next item
-var current = 0;                     // current item
-
-function setNext(activity, duration) {
-  if (index >= max) index = 0;
-  activities[index] = activity;
-  durations[index] = duration;
-
-  console.log(activities[index]);
-  console.log(durations[index]);
-
-  index++;
-}
-
-function getNextActivity() {
-  if (next >= max) next = 0;
-  console.log("getNextActivity " + activities[next] + " " + next);
-  current = next;
-  next++;
-
-  return activities[current]
-}
-
-function getNextDuration() {
-  console.log("getNextDuration " + durations[current] + " " + current);
-  return durations[current];
-}
-
-window.onload = function () {
-  var chart = new CanvasJS.Chart("chartContainer", "chartOptios");
-}
-
-function setChartOptions() {
-  var CanvasJS = require('canvasjs');
-  var chart = new CanvasJS.Chart("chartContainer", {
-    title: {
-      text: "Past 7 Workouts"
-    },
-    animationEnabled: true,
-    axisY: {
-      includeZero: true,
-      suffix: "mins"
-    },
-    data: [{
-      type: "bar",
-      indexLabel: "{y}",
-      yValueFormatString: "#,###mins",
-      dataPoints: [
-        { label: getNextActivity(), y: getNextDuration() },
-        { label: getNextActivity(), y: getNextDuration() },
-        { label: getNextActivity(), y: getNextDuration() },
-        { label: getNextActivity(), y: getNextDuration() },
-        { label: getNextActivity(), y: getNextDuration() },
-        { label: getNextActivity(), y: getNextDuration() },
-        { label: getNextActivity(), y: getNextDuration() }
-      ]
-    }]
-  });
-  chart.render();
-
-  console.log(chartOptions);
-}
-
-function logActivity() {
-
-  var theActivity = activity.value;
-  var theDuration = (document.slider.duration.value);
-  var theMessage = (document.getElementById("logMessage"));
-
-  theMessage.innerHTML = "";   // Clear message
-
-  console.log(theActivity);
-  console.log(theDuration);
-
-  setNext(theActivity, theDuration);
-
-  theMessage.innerHTML = "Activity '" + theActivity + "' for " + theDuration + " minutes has been logged.";
-}
-
 function calculateBMI() {
-  //const heightUnit = document.getElementById("height-unit").value;
-  //const weightUnit = document.getElementById("weight-unit").value;
+  var form = document.getElementById("form");
+  var age = document.getElementById("age");
+  var heightft = document.getElementById("heightft");
+  var heightin = document.getElementById("heightin");
+  var weight = document.getElementById("weight");
+  var walking = document.getElementById("walking");
+  var cardio = document.getElementById("cardio");
+  var male = document.getElementById("m");
+  var female = document.getElementById("f");
+  var gender = "";
+
+  console.log('Age: ' + age.value);
+  console.log('Height (feet): ' + heightft.value);
+  console.log('Height (+inches): ' + heightin.value);
+  console.log('Weight: ' + weight.value);
+  console.log('Walking: ' + walking.value);
+  console.log('Cardio: ' + cardio.value);
+  console.log('Male: ' + male.checked);
+  console.log('Female: ' + female.checked);
+
+  if (age.value != '' && isNaN(age.value)) {
+    alert("Age must be numeric");
+  }
+
+  if (heightft.value == '' || weight.value == '') {
+    alert("Height and Weight values are required");
+  }
+
+  if (isNaN(heightft.value)) {
+    alert("Height must be numeric");
+  }
+
+  if (heightft.value > 9) {
+    alert("Height value must be less than 9 feet");
+  }
+  var height = parseInt(heightft.value) * 12;  // Convert height to inches
+
+  if (heightin.value != '') {
+    if (isNaN(heightin.value)) {
+      alert("Height (inches) value must be numeric");
+    } else {
+      if (heightin.value > 11) {
+        alert("Height (inches) value must be less than 12");
+      }
+      height += parseInt(heightin.value);      // Add any addional inches to height
+    }
+  }
+
+  if (isNaN(weight.value)) {
+    alert("Weight must be numeric");
+  }
+
+  if (male.checked) {
+    gender = "male";
+  } else if (female.checked) {
+    gender = "female"
+  }
+  console.log("gender: " + gender);
+
+  if (walking.value != '' && isNaN(walking.value)) {
+    alert("Walking value must be numeric");
+  }
+
+  if (cardio.value != '' && isNaN(cardio.value)) {
+    alert("Cardiovascular activity value must be numeric");
+  }
 
   let textColor = "green";
   let remark = "Normal";
 
-  var theWeight = (document.weightbar.weight.value);
+  var theWeight = parseInt(weight.value);
   theWeight /= 2.205;                                  //  Pounds to kilograms
-  var theHeight = (document.heightbar.height.value);
+
+  var theHeight = height;
   theHeight /= 39.37;                                  // Inches to meters
   var theBMI = (theWeight) / Math.pow(theHeight, 2);   // weight / (height * height)
   var theRoundedBMI = theBMI.toFixed(1);
 
   console.log('Weight: ' + theWeight + " kg");
   console.log('Height: ' + theHeight + " m");
+  console.log("Height (inches): " + height);
   console.log('BMI: ' + theBMI);
 
   var bmiValue = document.getElementById("bmiValue");
@@ -110,11 +93,49 @@ function calculateBMI() {
   } else if (theBMI >= 25 && theBMI <= 29.9) {
     remark = "Overweight";
     textColor = "orange";
-  } else if (theBMI >= 30 && theBMI <= 34.9) {
+  } else if (theBMI >= 30) {
     remark = "Obese";
     textColor = "red";
   }
 
-  bmiValue.innerHTML = " Your BMI: " + theRoundedBMI + " (" + remark + ")"
+  bmiValue.innerHTML = " Your BMI: " + theRoundedBMI + " (" + remark + ")";
   bmiValue.style.color = textColor;
+
+  var bmr = parseInt(10 * theWeight + 6.25 * theHeight - 5 * parseInt(age.value)) + (gender === "male" ? 5 : -161);
+  bmr = bmr * 1.2;
+  bmr += parseInt(walking.value) * 60 * (.03 * parseInt(weight.value) * 1 / 0.45) / 7;
+  bmr += parseInt(cardio.value) * 60 * (.07 * parseInt(weight.value) * 1 / 0.45) / 7;
+  bmr = Math.floor(bmr);
+
+  var targetGainWeight = Math.round((bmr + 300) / 100) * 100;
+  var targetMaintainWeight = Math.round((bmr) / 100) * 100;
+  var targetLoseWeight = Math.round((bmr - 500) / 100) * 100;
+
+  var gainValue = document.getElementById("gainValue");
+  var maintainValue = document.getElementById("maintainValue");
+  var loseValue = document.getElementById("loseValue");
+
+  if (isNaN(targetGainWeight)) {
+    gainValue.innerHTML = "";
+  } else {
+    gainValue.innerHTML = "Daily calories required to GAIN weight: " + targetGainWeight;
+  }
+
+  if (isNaN(targetMaintainWeight)) {
+    maintainValue.innerHTML = "";
+  } else {
+    maintainValue.innerHTML = "Daily calories required to MAINTAIN current weight: " + targetMaintainWeight;
+  }
+
+  if (isNaN(targetLoseWeight)) {
+    loseValue.innerHTML = "";
+  } else {
+    loseValue.innerHTML = "Daily calories required to LOSE weight: " + targetLoseWeight;
+  }
+
+  console.log("Gain: " + targetGainWeight);
+  console.log("Lose: " + targetLoseWeight);
+  console.log("Maintain: " + targetMaintainWeight);
+
+  form.reset();
 }
